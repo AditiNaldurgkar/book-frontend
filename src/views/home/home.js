a5a9f450e694b99fa23258010adb047678f4d622
AditiNaldurgkar <aditinaldurgkar03@gmail.com>
2024-12-26 23:48:47 +0530

    first get api

--- src/views/home/home.js
+++ src/views/home/home.js
@@ -1,8 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from "axios"
+import { data } from 'react-router-dom';
 
 function Home() {
+    const [Books,setBooks] = useState([""])
+    useEffect(() => {
+        loadbooks(); 
+    }, []);
+    const loadbooks=async ()=>{ 
+        const response = await axios.get("http://localhost:5000/books") 
+        setBooks(response.data)
+    }
   return (
-    <div>home</div>
+    <div>
+         <div>
+            <h1>Home</h1>
+            {Books.length > 0 ? (
+                <ul>
+                    {Books.map((book, index) => (
+                        <li key={index}>{book.name}</li>
+                    ))}
+                </ul>
+            ) : (
+                <p>No books available or loading...</p>
+            )}
+        </div>
+    </div>
+    
   )
 }
 
