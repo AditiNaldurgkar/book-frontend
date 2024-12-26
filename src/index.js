8a2f04ba56210410a4af5aa7b5a50913481405ed
AditiNaldurgkar <aditinaldurgkar03@gmail.com>
2024-12-26 23:25:33 +0530

    home created

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import react from "react";
 import { createBrowserRouter,RouterProvider } from "react-router-dom";
 import { createRoot } from "react-dom/client";
+import Home from "./views/home/home";
 const root =createRoot(document.getElementById("root"));
 const router=createBrowserRouter([{
     path:'/',
-    element:<h1>hie</h1>
+    element:<Home/>
 
 }])
 root.render(<RouterProvider router={router}></RouterProvider>);
