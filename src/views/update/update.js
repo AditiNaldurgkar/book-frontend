dffb264aa7810206634c0316bf476961c098ac81
AditiNaldurgkar <aditinaldurgkar03@gmail.com>
2025-02-13 22:25:59 +0530

    some errors corrected

--- src/views/update/update.js
+++ src/views/update/update.js
@@ -1,32 +1,49 @@
-const Updatedetail = async (name) =>{
-    try{
-      await axios.put(`http://localhost:5000/books/${name}`)
-    }
-    catch(error){
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { useParams } from 'react-router-dom';
+
+function Update() {
+  const { name: paramName } = useParams(); 
+  const [name, setName] = useState(paramName || ""); 
+  const [price, setPrice] = useState(""); 
+
+  useEffect(() => {
+    axios.get(`http://localhost:5000/books/${paramName}`)
+      .then(response => {
+        setName(response.data.name);
+        setPrice(response.data.price);
+      })
+      .catch(error => console.error(error));
+  }, [paramName]); 
+  const updateDetail = async () => {
+    try {
+      await axios.post(`http://localhost:5000/books/${name}`,{name,price});
+      alert("Book details updated successfully!");
+    } catch (error) {
       console.error(error);
     }
-  }
-  import React, { useEffect, useState } from 'react'
-  import axios from 'axios';
-  const [Name,setName] = useState("")
-  function Update({name}) {
-    useEffect(()=>{
-         
-        return axios.get(`http://localhost:5000/books/${name}`);
-      },[name]);
-    return (
-     <div>
+  };
+
+  return (
+    <div>
       <form>
-        <label>Enter book name :</label>
-        <input id='name' value={name} onChange={setName(name)}>
-        </input>
-        <br/>
-        <label>Enter book price :</label>
-        <input id='price'></input>
+        <label>Enter book name:</label>
+        <input 
+          id="name" 
+          value={name} 
+          onChange={(e) => setName(e.target.value)}
+        />
+        <br />
+        <label>Enter book price:</label>
+        <input 
+          id="price" 
+          value={price} 
+          onChange={(e) => setPrice(e.target.value)}
+        />
       </form>
-      <button onClick={Updatedetail}>Submit</button>
-      </div>
-    )
-  }
-  
-  export default Update
+      <button onClick={updateDetail}>Submit</button>
+    </div>
+  );
+}
+
+export default Update;
